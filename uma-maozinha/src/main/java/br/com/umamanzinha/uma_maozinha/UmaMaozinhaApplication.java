dab88d30d8d390417ec5e2d2ebd971a4344cdafe
ProgGusta <gustavoprogpc@gmail.com>
2025-10-04 21:41:14 -0300

    spring initializr and gitignore

---
+++ uma-maozinha/src/main/java/br/com/umamanzinha/uma_maozinha/UmaMaozinhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.umamanzinha.uma_maozinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmaMaozinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmaMaozinhaApplication.class, args);
+	}
+
+}
---
+++ uma-maozinha/src/test/java/br/com/umamanzinha/uma_maozinha/UmaMaozinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.umamanzinha.uma_maozinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmaMaozinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
