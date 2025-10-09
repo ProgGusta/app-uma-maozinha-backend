f913a3d452d7739997bb61d6ac69d98be9331e45
Brenno-SF <brprogrammersf@gmail.com>
2025-10-08 23:24:00 -0300

    feat: adds services entity

---
+++ uma-maozinha/src/main/java/br/com/umamanzinha/uma_maozinha/entities/Services.java
@@ -0,0 +1,49 @@
+package br.com.umamanzinha.uma_maozinha.entities;
+
+import br.com.umamanzinha.uma_maozinha.enums.ServiceStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "services")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Services {
+    @Id
+    @Column(name = "service_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private ServiceStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(length = 255)
+    private String location;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "freelancer_profile_id_fk", nullable = false)
+    private FreelancerProfile freelancerProfile;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id_fk", nullable = false)
+    private User user;
+
+}
---
+++ uma-maozinha/src/main/java/br/com/umamanzinha/uma_maozinha/enums/ServiceStatus.java
@@ -0,0 +1,8 @@
+package br.com.umamanzinha.uma_maozinha.enums;
+
+public enum ServiceStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
